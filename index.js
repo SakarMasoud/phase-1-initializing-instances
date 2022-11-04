d9a65c3fdc5d5b1f761d20087c1bbbba081781c5
Sakar Masoud <masoudsakar@gmail.com>
2022-11-04 11:41:46 +0300

    Sakar Masoud

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code here
+class Breakfast{
+    food;
+    drink;
+    constructor (food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch{
+    salad;
+    soup;
+    drink;
+    constructor (salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner{
+    salad;
+    soup;
+    entree;
+    constructor (salad, soup, entree){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+    }
+}
